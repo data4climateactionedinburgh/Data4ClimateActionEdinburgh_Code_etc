95c9956abd48ea3968b6e2c831a656f8cf0557c2
PaulineDataWard <polo@paulineward.me.uk>
2025-11-21 22:07:46 +0000

    rain data

---
+++ code/rainfall_prep.R
@@ -0,0 +1,7 @@
+# Get and prepare rainfall data for D4CAE dashboard
+
+library(tidyverse)
+
+# SEPA API allows download in this form:
+# {baseurl}/api/Daily/{id}?csv=true&all=true
+# CSV : {baseurl}/api/Daily/{id}?csv=true&all=true
