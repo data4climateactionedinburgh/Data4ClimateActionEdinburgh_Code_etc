3ed5e2c946fda00669197fdda4bc17aac2c5f957
PaulineDataWard <polo@paulineward.me.uk>
2025-11-22 08:55:58 +0000

    fresh rain data files and import code

--- code/rainfall_prep.R
+++ code/rainfall_prep.R
@@ -1,7 +1,25 @@
 # Get and prepare rainfall data for D4CAE dashboard
 
 library(tidyverse)
+library(here)
 
 # SEPA API allows download in this form:
 # {baseurl}/api/Daily/{id}?csv=true&all=true
 # CSV : {baseurl}/api/Daily/{id}?csv=true&all=true
+
+Edin_stations <- read_csv(
+    here(
+        "data",
+        "rainfall",
+        "rain_stations_edinburgh.csv"
+    ),
+    col_type = list(.default = col_character())
+)
+
+aggreg_edinburgh_rainfall <- tibble()
+
+rainfiles <- tibble("filename" = list.files(here("data", "rainfall"))) |>
+    filter(str_detect(filename, "\\.csv"))
+
+monthly_rainfiles <- rainfiles |>
+    filter(str_detect(filename, "monthly"))
